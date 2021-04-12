1553693175f62a9e1a4b6691137f687d6c107d1c
Felis94 <felistigris13@gmail.com>
2021-04-12 22:20:39 +0700

    my LoopFor task
    menentukan hasil bilangan berpangkat

--- src/id/felis/modul0/LoopFor.java
+++ src/id/felis/modul0/LoopFor.java
@@ -1,4 +1,24 @@
 package id.felis.modul0;
 
+import java.util.Scanner;
+
 public class LoopFor {
+    /*
+    PROGRAM UNTUK MENENTUKAN BILANGAN BERPANGKAT
+     */
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner (System.in);
+        int hasil = 1;
+
+        System.out.print("Masukkan bilangan\t\t: ");
+        int bil = s.nextInt();
+        System.out.print("Masukkan pencacahan\t\t: ");
+        int pangkat = s.nextInt();
+
+        for (int i = 0; i < pangkat; i++) {
+            hasil *= bil;
+        }
+        System.out.print("Hasil pangkat\t\t\t: " + hasil);
+    }
 }
