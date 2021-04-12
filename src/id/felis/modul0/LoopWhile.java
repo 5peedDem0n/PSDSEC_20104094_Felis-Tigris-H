12b4b38396565dced8277b9f37e7a4a859ea5450
Felis94 <felistigris13@gmail.com>
2021-04-12 22:10:32 +0700

    my LoopWhile task
    Make a right triagle with numbers

--- src/id/felis/modul0/LoopWhile.java
+++ src/id/felis/modul0/LoopWhile.java
@@ -1,4 +1,27 @@
 package id.felis.modul0;
 
+import java.util.Scanner;
+
 public class LoopWhile {
+    /*
+    PROGRAM PERKALIAN INCREMENT KESAMPING DAN DECREMENT KEBAWAH
+    MEMBENTUK SEGITIGA SIKU - SIKU
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        int a, b;
+
+        System.out.print("Masukkan bilangan positif : ");
+        a = s.nextInt();
+
+        while (a >= 1){
+            b = 1;
+            while (b <= a){
+                System.out.println(b * a + "");
+                b++;
+            }
+            System.out.println();
+            a--;
+        }
+    }
 }
